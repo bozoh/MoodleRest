62ff2cdad0330f2b3192aa25266b6e5827f5ab55
Carlos Alexandre Soares da Fonseca <carlos.alexandre@outlook.com>
2016-07-05 12:08:10 -0400

    Refactoring packages

---
+++ src/main/java/net/beaconhillcott/moodlerest/commons/MoodleCohortException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.beaconhillcott.moodlerest.commons;
+
+/**
+ *
+ * @author root
+ */
+public class MoodleCohortException extends Exception {
+
+  public MoodleCohortException() {
+  }
+  
+}
---
+++ src/main/java/net/beaconhillcott/moodlerest/commons/MoodleGroupGroupingsException.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.beaconhillcott.moodlerest.commons;
+
+/**
+ *
+ * @author root
+ */
+class MoodleGroupGroupingsException extends Exception {
+  
+}
